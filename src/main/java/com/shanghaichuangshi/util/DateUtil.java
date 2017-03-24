d8dae7e8cfdf409fc2a58eabf999dd578ea23bab
NowUI <nowui@163.com>
2017-03-24 18:41:47 +0800

    2017-03-24 18:41

--- src/main/java/com/shanghaichuangshi/util/DateUtil.java
+++ src/main/java/com/shanghaichuangshi/util/DateUtil.java
@@ -6,8 +6,9 @@ import java.util.Date;
 
 public class DateUtil {
 
-    private static final SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    public static final SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    public static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    public static final SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
 
     public static String getDateString(Date dateTime) {
         return dateFormat.format(dateTime);
