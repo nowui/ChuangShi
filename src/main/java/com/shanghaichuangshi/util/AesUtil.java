f466d64de9be495997b55a96cc9150f623b51a5c
NowUI <nowui@163.com>
2017-05-09 04:06:42 +0800

    2017-05-09 04:06

--- src/main/java/com/shanghaichuangshi/util/AesUtil.java
+++ src/main/java/com/shanghaichuangshi/util/AesUtil.java
@@ -3,18 +3,86 @@ package com.shanghaichuangshi.util;
 import com.alibaba.druid.util.StringUtils;
 import com.shanghaichuangshi.constant.Constant;
 import org.apache.commons.codec.binary.Base64;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import sun.misc.BASE64Decoder;
 
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+import java.security.*;
+import java.security.spec.InvalidParameterSpecException;
 
 public class AesUtil {
+
+    static {
+        System.out.println("999999999999999999999999999999999999999999999999999999999999");
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
     /**
      * 算法
      */
     private static final String ALGORITHMSTR = "AES/ECB/PKCS5Padding";
+
+    /**
+     * AES解密
+     *
+     * @param data           //密文，被加密的数据
+     * @param key            //秘钥
+     * @param iv             //偏移量
+     * @param encodingFormat //解密后的结果需要进行的编码
+     * @return
+     * @throws Exception
+     */
+    public static String decrypt(String data, String key, String iv, String encodingFormat) throws Exception {
+//        initialize();
+
+        //被加密的数据
+        byte[] dataByte = Base64.decodeBase64(data);
+        //加密秘钥
+        byte[] keyByte = Base64.decodeBase64(key);
+        //偏移量
+        byte[] ivByte = Base64.decodeBase64(iv);
+
+
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
+
+            SecretKeySpec spec = new SecretKeySpec(keyByte, "AES");
+
+            AlgorithmParameters parameters = AlgorithmParameters.getInstance("AES");
+            parameters.init(new IvParameterSpec(ivByte));
+
+            cipher.init(Cipher.DECRYPT_MODE, spec, parameters);// 初始化
+
+            byte[] resultByte = cipher.doFinal(dataByte);
+            if (null != resultByte && resultByte.length > 0) {
+                String result = new String(resultByte, encodingFormat);
+                return result;
+            }
+            return null;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidParameterSpecException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 
     /**
      * aes解密
